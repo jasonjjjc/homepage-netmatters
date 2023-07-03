d91ea71393c1f2b99abf0b420192bc775551fc71
Jason Chalangary <jasonchalangary@Jasons-MacBook-Air.local>
2023-07-03 17:40:57 +0100

    most of the page content complete

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      screens: {
+        'c-sm': '481px', // Custom breakpoint at 481 pixels
+      },
+    },
+  },
+  plugins: [],
+}
+
