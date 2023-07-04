c570c486f8763dacd9ed74bd4a07e979172d7304
jasonjjjc <57393155+jasonjjjc@users.noreply.github.com>
2023-07-04 18:47:52 +0100

    containers in about section
    completed containers for the about section and added minimal styling.

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,10 +11,16 @@ module.exports = {
       },
       spacing: {
         '30': '7.5rem',
+        '2.5': '0.625rem',
+        '7': '1.75rem',
       },
       fontSize: {
         '4.5xl': '2.75rem',
         '2.5xl': '1.75rem',
+        '3.5xl': '2.25rem',
+      },
+      lineHeight: {
+        '11': '1.1',
       },
     },
   },
